8933c01ea9c3de19992542c84908a59d3ac03148
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-05-26 13:05:01 +0400

    passes lint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
-const readline = require('readline');
+/*eslint-disable*/
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+process.stdin.on('data', (data) => {
+	process.stdout.write(`Your name is: ${data}`);
 });
-console.log('Welcome to Holberton School, what is your name?');
-rl.on('line', (input) => {
-	console.log(`Your name is: ${input}`);
-	rl.close();
-});
-rl.on('close', () => {
-	console.log('This important software is now closing');
+
+process.stdin.on('end', () => {
+	process.stdout.write('This important software is now closing\n');
 });
